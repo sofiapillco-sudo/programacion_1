6cdccf6e3c9cd943d737b8f14b71884fdc00b822
sofiapillco-sudo <sofia.pillco@ucb.edu.bo>
2025-09-18 11:58:46 -0400

    Update ejercico_02_03.cpp

--- practica_02/ejercico_02_03.cpp
+++ practica_02/ejercico_02_03.cpp
@@ -2,7 +2,7 @@
 // Autor: Sofia Paola Pillco Vera
 // Carnet: 7346371 L.P.
 // Carrera del estudiante: Ingeniería industrial
-// Fecha creación: 08/09/2025
+// Fecha creación: 17/09/2025
 // Número de ejercicio: 3
 
 #include <iostream>
@@ -30,4 +30,5 @@ int main()
     cout << "factorial" << factorial(num) << endl;
     
     return 0;
+
 }
