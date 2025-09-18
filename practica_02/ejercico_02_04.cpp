57095ae7f0ed1b72c5ddca8870e5bc1dbeadeb19
sofiapillco-sudo <sofia.pillco@ucb.edu.bo>
2025-09-18 12:00:02 -0400

    Update ejercico_02_04.cpp
    Completo

--- practica_02/ejercico_02_04.cpp
+++ practica_02/ejercico_02_04.cpp
@@ -2,7 +2,7 @@
 // Autor: Sofia Paola Pillco Vera
 // Carnet: 7346371 L.P.
 // Carrera del estudiante: Ingeniería industrial
-// Fecha creación: 08/09/2025
+// Fecha creación: 17/09/2025
 // Número de ejercicio: 4
 
 #include <iostream>
@@ -51,4 +51,5 @@ int main()
     
 
     return 0;
+
 }
